a092bc6cc7e49c8aeb35e3a4084bc52d867b14c5
Eric Hendrix <agentmrbig@gmail.com>
2020-04-16 02:15:08 -0400

    Added cursor key movment, collision

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,6 +31,11 @@ function preload() {
     frameWidth: 32,
     frameHeight: 32,
   });
+
+  this.load.spritesheet('characters', 'assets/images/characters.png', {
+    frameWidth: 32,
+    frameHeight: 32,
+  });
 }
 
 function create() {
@@ -39,6 +44,36 @@ function create() {
   this.add.image(300, 300, 'items', 0);
 
   this.physics.add.image(500, 100, 'button1');
+
+  this.chest = this.physics.add.image(300, 300, 'items', 0);
+
+  this.wall = this.physics.add.image(500, 100, 'button1');
+  this.wall.setImmovable();
+
+  this.player = this.physics.add.image(32, 32, 'characters', 0);
+  this.player.setScale(2);
+  this.player.body.setCollideWorldBounds(true);
+
+  this.physics.add.collider(this.player, this.wall);
+  this.physics.add.overlap(this.player, this.chest, function () {
+    console.log('overlap');
+  });
+
+  this.cursors = this.input.keyboard.createCursorKeys();
 }
 
-function update() {}
+function update() {
+  this.player.setVelocity(0);
+
+  if (this.cursors.left.isDown) {
+    this.player.setVelocityX(-160);
+  } else if (this.cursors.right.isDown) {
+    this.player.setVelocityX(160);
+  }
+
+  if (this.cursors.up.isDown) {
+    this.player.setVelocityY(-160);
+  } else if (this.cursors.down.isDown) {
+    this.player.setVelocityY(160);
+  }
+}
