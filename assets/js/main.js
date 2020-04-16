f7aa3a5003b1ee8e2417360d7271c68e239a723d
Eric Hendrix <agentmrbig@gmail.com>
2020-04-16 01:56:18 -0400

    Added 'arcade' physics

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,44 @@
+// Essential Phaser config
 var config = {
   type: Phaser.AUTO,
   width: 800,
   height: 600,
   scene: {
+    init: init,
     preload: preload,
     create: create,
+    update: update,
+  },
+  physics: {
+    default: 'arcade',
+    arcade: {
+      debug: true,
+      gravity: {
+        y: 0,
+      },
+    },
   },
 };
 
 var game = new Phaser.Game(config);
 
-function preload() {}
+function init() {}
 
-function create() {}
+// Preload assets
+function preload() {
+  this.load.image('button1', 'assets/images/ui/blue_button01.png');
+  this.load.spritesheet('items', 'assets/images/items.png', {
+    frameWidth: 32,
+    frameHeight: 32,
+  });
+}
+
+function create() {
+  this.add.sprite(300, 100, 'button1');
+
+  this.add.image(300, 300, 'items', 0);
+
+  this.physics.add.image(500, 100, 'button1');
+}
+
+function update() {}
