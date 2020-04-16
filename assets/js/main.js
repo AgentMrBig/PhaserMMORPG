39f19734ac34213374280711b1b9814ef8830ba2
Eric Hendrix <agentmrbig@gmail.com>
2020-04-16 01:37:46 -0400

    Phaser running

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+var config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  scene: {
+    preload: preload,
+    create: create,
+  },
+};
+
+var game = new Phaser.Game(config);
+
+function preload() {}
+
+function create() {}
